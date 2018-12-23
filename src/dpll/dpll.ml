f23058edd82074c0d7a40d82fe3a76760f97c001
tyno43 <skriulle5@gmail.com>
2018-12-24 01:05:39 +0900

    add, update clause function

--- src/dpll/dpll.ml
+++ src/dpll/dpll.ml
@@ -1,1 +1,30 @@
-type puzzle = (int list) list;;
+type assign = string * bool;;
+
+type literal = | P of string | N of string;;
+type clause = literal list;;
+type cnf = clause list;;
+
+type alphabets = string list;;
+
+exception Satisfied;;
+
+let update_clause cla s b =
+  let rec judge s' h t b k =
+    if s = s' then
+      if b then raise Satisfied
+      else k t
+    else update t (fun x -> k (h::x))
+  and update cla k =
+    match cla with
+    | [] -> k []
+    | h::t ->
+      begin
+        match h with
+        | P s' -> judge s' h t b k
+        | N s' -> judge s' h t (not b) k
+      end
+  in
+  try
+    update cla (fun x -> x)
+  with | Satisfied -> []
+;;
--- tests/dpll_test.ml
+++ tests/dpll_test.ml
@@ -1,9 +1,18 @@
 open OUnit2
 open Dpll
 
+let update_clause_test =
+  "新しい割り当てが項に含まれていれば更新、なければ何もしない" >::
+    (fun _ ->
+      assert_equal [N "a"; P "b"; N "c"] (update_clause [N "a"; P "b"; N "c"; P "d"] "d" false); 
+      assert_equal [] (update_clause [N "a"; P "b"; N "c"; P "d"] "b" true); 
+      assert_equal [N "a"; P "b"; P "d"] (update_clause [N "a"; P "b"; N "c"; P "d"] "c" true); 
+      assert_equal [N "a"; P "b"; N "c"; P "d"] (update_clause [N "a"; P "b"; N "c"; P "d"] "f" false); 
+    )
+;;
 
 let tests =
-  "all_tests" >:::
-    [
-    ]
+  "all_tests" >::: [
+    update_clause_test;
+  ]
 ;;
